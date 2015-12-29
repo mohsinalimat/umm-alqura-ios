159b74df19111fdb75d403553bc7552ca5da59c4
kaluaim <kaluaim@gmail.com>
2015-12-29 03:13:05 +0300

    adding UmmAlQuraManager class

---
+++ UmmAlQura/UmmAlQuraManager.h
@@ -0,0 +1,13 @@
+//
+//  UmmAlQuraManager.h
+//  UmmAlQura
+//
+//  Created by Khalid Alnuaim on 12/29/15.
+//  Copyright © 2015 KACST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UmmAlQuraManager : NSObject
+
+@end
---
+++ UmmAlQura/UmmAlQuraManager.m
@@ -0,0 +1,13 @@
+//
+//  UmmAlQuraManager.m
+//  UmmAlQura
+//
+//  Created by Khalid Alnuaim on 12/29/15.
+//  Copyright © 2015 KACST. All rights reserved.
+//
+
+#import "UmmAlQuraManager.h"
+
+@implementation UmmAlQuraManager
+
+@end
