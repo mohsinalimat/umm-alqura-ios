6536ac06b5f9a0171ed443d2c91061cde534991e
kaluaim <kaluaim@gmail.com>
2016-02-24 05:33:08 +0300

    adding qiblah code and lib

--- UmmAlQura/QiblahVC.m
+++ UmmAlQura/QiblahVC.m
@@ -2,10 +2,12 @@
 #import "UmmAlQuraManager.h"
 #import "UmmAlQuraUtilities.h"
 #import "AppConstants.h"
+#import "GeoPointCompass.h"
 
 @interface QiblahVC ()
 @property (strong, nonatomic) UmmAlQuraManager      *ummAlQuraManager;
 @property (strong, nonatomic) UmmAlQuraUtilities    *ummAlQuraUtilities;
+@property (strong, nonatomic) GeoPointCompass       *geoPointCompass;
 @end
 
 @implementation QiblahVC
@@ -19,10 +21,18 @@
         _ummAlQuraManager.locationManager.delegate = self;
         [_ummAlQuraManager.locationManager requestWhenInUseAuthorization];
     }
+    
+    _geoPointCompass = [[GeoPointCompass alloc] init];
+    [_geoPointCompass setArrowImageView:_needle];
+    _geoPointCompass.latitudeOfTargetedPoint = 0.373965869;
+    _geoPointCompass.longitudeOfTargetedPoint = 0.6951937182;
 
 }
 
 - (IBAction)done {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
+
 @end
